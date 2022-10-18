ae5bbf6250ae4819d73b8307118ec44729d27c8a
Nightlymist <nathalieaouellette@gmail.com>
2022-10-18 11:49:41 -0400

    Bonus done

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -1,42 +1,65 @@
 -- Challenge Bonus queries.
 -- 1. (2.5 pts)
 -- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
-SELECT cf_id,
-	pledged
-FROM campaign
+SELECT c.cf_id,
+	c.backers_count,
+	c.outcome
+FROM campaign as c
+WHERE (c.outcome = 'live')
 GROUP BY cf_id
-ORDER BY cf_id DESC;
+ORDER BY backers_count DESC;
 
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
-SELECT * 
-FROM backers
-ORDER BY cf_id DESC;
+SELECT c.backers_count,
+	c.outcome,
+	b.cf_id
+FROM backers as b
+	JOIN campaign as c
+		ON (b.cf_id = c.cf_id)
+WHERE (c.outcome = 'live')
+GROUP BY b.cf_id
+ORDER BY c.backers_count DESC;
 
 
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
-SELECT b.first_name,
-	b.last_name,
-	b.email,
-	
-FROM backers as b
+SELECT co.first_name,
+	co.last_name,
+	co.email,
+	(c.goal - c.pledged) as "Remaining Goal Amount"
 INTO email_contacts_remaining_goal_amount
-ORDER BY Remaining Goal Amount DESC;
+FROM contacts as co
+	JOIN campaign as c
+		ON (co.contact_id = c.contact_id)
+WHERE (c.outcome = 'live')
+ORDER BY "Remaining Goal Amount" DESC;
 
 
 
 -- Check the table
-
+SELECT * FROM email_contacts_remaining_goal_amount
 
 -- 4. (5 pts)
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
-
-
+SELECT b.email,
+	b.first_name,
+	b.last_name,
+	b.cf_id,
+	c.company_name,
+	c.description,
+	c.end_date,
+	(c.goal - c.pledged) as "Left of Goal"
+INTO email_backers_remaining_goal_amount
+FROM campaign AS c
+	JOIN backers AS b
+		ON (c.cf_id = b.cf_id)
+WHERE (c.outcome = 'live')
+ORDER BY b.last_name, b.first_name, b.email DESC;
 
 -- Check the table
-
+SELECT * FROM email_backers_remaining_goal_amount;
 

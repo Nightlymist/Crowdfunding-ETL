eb737987d1585ac6d314c84f3dc73fe767cab9ed
Nightlymist <nathalieaouellette@gmail.com>
2022-10-17 21:47:49 -0400

    finally fixing the backers foreign

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -1,18 +1,29 @@
 -- Challenge Bonus queries.
 -- 1. (2.5 pts)
 -- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
-
-
+SELECT cf_id,
+	pledged
+FROM campaign
+GROUP BY cf_id
+ORDER BY cf_id DESC;
 
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
-
+SELECT * 
+FROM backers
+ORDER BY cf_id DESC;
 
 
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
-
+SELECT b.first_name,
+	b.last_name,
+	b.email,
+	
+FROM backers as b
+INTO email_contacts_remaining_goal_amount
+ORDER BY Remaining Goal Amount DESC;
 
 
 
